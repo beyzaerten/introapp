d391423edd5159d1e297b9c1382e197ddd67e8fe
Beyza Erten <ertenbeyza54@gmail.com>
2023-10-31 14:39:22 +0300

    imageAsset

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 209, 125, 255),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "BEYZA ERTEN",
-              style: TextStyle(fontSize: 25, color: Colors.white),
-            ),
-            Text(
-              "Tobeto - Mobil Geliştirici - 1A",
-              style: TextStyle(fontSize: 20, color: Colors.white),
-            ),
-            Text(
-              "31.10.2023",
-              style: TextStyle(fontSize: 20, color: Colors.white),
-            ),
-          ],
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 209, 125, 255),
+        body: Center(
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Dikey olarak ortalama
+            children: [
+              Image.asset(
+                'assets/beyz.jpeg',
+                width: 120,
+                height: 120,
+              ),
+              SizedBox(height: 10), // Metinler arasında bir boşluk ekler
+              Text(
+                "BEYZA ERTEN",
+                style: TextStyle(fontSize: 25, color: Colors.white),
+              ),
+              Text(
+                "Tobeto - Mobil Geliştirici - 1A",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              Text(
+                "31.10.2023",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ),
     ),
-  ));
+  );
 }
